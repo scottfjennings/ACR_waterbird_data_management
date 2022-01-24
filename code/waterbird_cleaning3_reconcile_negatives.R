190d0e95b45e2ef2156cf7769591a98334cfa9b7
scottfjennings <scott.f.jennings@gmail.com>
2022-01-24 08:58:34 -0800

    functions for more correct handling of negatives.

--- code/waterbird_cleaning3_reconcile_negatives.R
+++ code/waterbird_cleaning3_reconcile_negatives.R
@@ -1,29 +1,17 @@
 
 
-# this is the third of 4 steps to prep waterbird data for use.
 
-# this file carries negative tallies forward through sections, and if there are net negatives left over after section 4, these are added back in to the section 4 count.
+get_section_pos_neg <- function(df) {
+  df <- df %>% 
+    mutate(pos.count = ifelse(count > 0, count, 0),
+           neg.count = ifelse(count < 0, count, 0)) %>% 
+    group_by(date, alpha.code, section) %>% 
+    summarise(positive = sum(pos.count),
+           negative = sum(neg.count)) %>% 
+    ungroup()
+}
 
-# note that waterbird_cleaning1 is sourced below to read the data from access and do basic house-keeping on variable names
-
-# section.num = row 11
-# count = yellow boxes
-# final.data.record = pink boxes, for section 4 = column I
-# section.sum = row 24 under yellow boxes
-# section.sum.final = row 24 under pink boxes
-# carried.forward = row 25
-
-
-# source, loads packages, data ----
-
-
-# define functions ----
-
-# reproducing row 24 of the xlsx spreadsheet
-# calculate total positive birds ("section.final") and total positive minus negative ("section.tally)
-
-
-#' Widen block tallies
+#' Widen block positive/negative
 #' 
 #' Transform long version block tallies to wide version with columns for each block
 #'
@@ -38,125 +26,72 @@
 #' 
 #' wide_blocks <- widen_block_tallies(wbirds)
 #' 
-widen_block_tallies <- function(df) {
+widen_block_pos_neg <- function(df) {
 df_wider <- df %>% 
-  select(date, alpha.code, block, section, transect, count) %>%
-  mutate(section = paste("blockcount.", section, sep = "")) %>% 
-  pivot_wider(id_cols = c(date, transect, alpha.code), names_from = section, values_from = count) %>% 
+  pivot_wider(id_cols = c(date, alpha.code), names_from = section, values_from = c("positive", "negative")) %>% 
   replace(is.na(.), 0) %>% 
-  arrange(date, alpha.code, transect)
-
-return(df_wider)
-}
-
-
-#' Widen section tallies
-#' 
-#' Transform long version section tallies to wide version with columns for each section.
-#'
-#' @param df data frame with section tally data. 
-#'
-#' @return
-#' @export
-#' 
-#' @details Function is designed and intended to be run on a data frame with as many POOLED birds allocated to constituent species as possible (using workflow/functions defined in waterbird_cleaning2_split_groups.R). However, this can also be run on data that doesn't have POOLED birds allocated, if the user has a good reason to do so.
-#'
-#' @examples
-widen_section_tallies <- function(df) {
-df_longer <- df %>% 
-  pivot_longer(cols = contains("section."), names_to = "sum.type", values_to = "sum") %>% 
-  mutate(sec.name = paste(sum.type, section, sep = "_")) 
-
-
-df_wider <- df_longer %>% 
-  pivot_wider(id_cols = c(date, alpha.code), names_from = sec.name, values_from = sum) %>% 
-  replace(is.na(.), 0) %>% 
-  select(date, alpha.code, contains("1"), contains("2"), contains("3"), contains("4")) 
+  arrange(date, alpha.code)
 
 return(df_wider)
 }
 
 
 
-#' Calculate carried forward tallies
+
+
+
+
+#' Calculate birds to subtract forward and/or add backward
 #'
 #' @param df data frame with section tallies in wide format (output from widen_section_tallies())
 #'
 #' @return
 #' @export
 #' 
-#' @details This calculates the "Cumulative Net Field Tally (including "Carried Forward" Negative Tally)" in row 25 of the xlsx files. This calculation is the way any negative birds in a given section are carried forward to the next section. The value is simply the sum of all birds for that species (positives and negatives) and whatever value was carried forward from previous sections. 
+#' @details This is an attempt to fix some problems in the logic of the original negative machine (and calc_carried_forward() which duplicates negative treatment of the negative machine).
 #' 
-#' Note, there is a different rule for calculating section 1 carried forward than for the remaining sections.
+#' we assume all birds flying past the boats originated on the bay (did not arrive to the bay behind the boats from the south). Thus, all birds flying past the boats have either already been counted or they were missed in an area that was counted. Either way, all negatives should be subtracted from future section counts, and they should never be added to section 4. If the negatives can be explained by positives in each section they should be assigned to  the section they were observed in. if they cannot be explained by previous positives they should be added back to section 1 (most parsimonious assumption is that they were missed in precount or moved south between end of precount and start of boat count).
 #' 
-#' Also note, this appears to treat carried forward negatives incorrectly. Net carried forward negatives that remain upon reaching section 4 are added back to the section 4 tally. However, if there were more positives than the net negatives before the negatives were encountered then those negatives were likely already counted and thus are double counted when they're added back into section 4. On the other hand, if those negatives cannot be explained by previously encountered positives, then we must conclude that those birds either came into the bay from the south or were missed on the first count. In this case those negatives are most appropriately added to the tally for a section farther south in the bay than section 4. Perhaps they should be added to the inverness/millerton precount areas? 
+#' set section.1.final to whichever is greater between the sum of the section 1 positives and the absolute value of the sum of the section 1 negatives.  
+#' set section.1.forward to the net negative in section 1, or 0, whichever is smaller
+#' set section.2.final to section.2.tally minus section.1.forward (any net negatives from section 1 have already been counted, so they should be subtracted from subsequent sections). If this value is less than 0, then set section.2.final = 0
+#' set section.2.forward to section.2. 
 #'
 #' @examples
-calc_carried_forward <- function(df) {
+subtract_forward_add_back <- function(df) {
   
-df <- negatives_carried
-  
-tallies_carried_forward <- df %>% 
-  group_by(date, alpha.code) %>% 
-  mutate(section.1.tally = sum(blockcount.sec1),
-         section.2.tally = sum(blockcount.sec2),
-         section.3.tally = sum(blockcount.sec3),
-         section.4.tally = sum(blockcount.sec4)) %>% 
-  ungroup() %>% 
-  mutate(section.1.final = ifelse(section.1.tally >= 0, section.1.tally, 0),
-         section.1.final = ifelse(section.1.tally >= 0, section.1.tally, 0),
-         )
-  select(date, transect, alpha.code, contains("1"), contains("2"), contains("3"), contains("4")) 
 
-}
-
-
-#' Calculate carried forward tallies
-#'
-#' @param df data frame with section tallies in wide format (output from widen_section_tallies())
-#'
-#' @return
-#' @export
-#' 
-#' @details This calculates the "Cumulative Net Field Tally (including "Carried Forward" Negative Tally)" in row 25 of the xlsx files. This calculation is the way any negative birds in a given section are carried forward to the next section. The value is simply the sum of all birds for that species (positives and negatives) and whatever value was carried forward from previous sections.
-#'
-#' @examples
-subtract_forward <- function(df) {
-  
-tallies_carried_forward <- df %>% 
-  mutate(net.pos.sec2 = section.final_1 + section.final_2,
-         net.neg.sec2 = ifelse(section.tally_1 + section.tally_2 < 0, section.tally_1 + section.tally_2, 0)) %>% 
-  select(date, alpha.code, contains("1"), contains("2"), contains("3"), contains("4")) 
-  
-  
-  
-  
-  mutate(out_sec1 = ifelse(section.tally_1 < 0, abs(section.tally_1), section.final_1),
-         forward_sec1 = ifelse(section.tally_1 < 0, section.tally_1, 0),
-         out_sec2 = ifelse(forward_sec1 + section.tally_2 > 0, forward_sec1 + section.tally_2, 0),
-         forward_sec2 = ifelse(forward_sec1 + section.tally_2 < 0, forward_sec1 + section.tally_2, 0),
-         addback_sec1 = ifelse(section.tally_2 < 0 & section.final_2 > 0, abs(section.tally_2), 0),
-         out_sec3 = ifelse(forward_sec2 + section.tally_3 > 0, forward_sec2 + section.tally_3, 0),
-         forward_sec3 = ifelse(forward_sec2 + section.tally_3 < 0, forward_sec2 + section.tally_3, 0),
-         out_sec4 = ifelse(forward_sec3 + section.tally_4 > 0, forward_sec3 + section.tally_4, 0),
-         forward_sec4 = ifelse(forward_sec3 + section.tally_4 < 0, forward_sec3 + section.tally_4, 0)) %>% 
+subtracted_forward <- df %>% 
+  mutate(draft_sec1 = positive_sec1,
+         add.backward_sec1 = abs(negative_sec1),
+         #add.backward_sec1 = ifelse(positive_sec1 + negative_sec1 < 0, abs(positive_sec1 + negative_sec1), 0), pretty sure no ifelse needed here, should just be the section 1 negatives
+         known.ahead_sec1 = negative_sec1,
+         known.behind_sec1 = positive_sec1,
+         # section 2
+         draft_sec2 = ifelse(positive_sec2 + known.ahead_sec1 >= 0, positive_sec2 + known.ahead_sec1, 0), # subtract any neg_sec1, if more neg_sec1 than pos_sec2, draft_sec2 = 0
+         add.backward_sec2 = add.backward_sec1 + ifelse(known.behind_sec1 < abs(negative_sec2), abs(known.behind_sec1 + negative_sec2), 0),
+         known.ahead_sec2 = negative_sec2 + ifelse(known.ahead_sec1 + positive_sec2 > 0, 0, known.ahead_sec1 + positive_sec2),
+         #known.behind_sec2 = positive_sec2 + ifelse(draft_sec1 + negative_sec2 > 0, draft_sec1 + negative_sec2, 0),
+         known.behind_sec2 = positive_sec2 + ifelse(known.behind_sec1 + negative_sec2 > 0, known.behind_sec1 + negative_sec2, 0),
+         # section 3
+         draft_sec3 = ifelse(positive_sec3 + known.ahead_sec2 >= 0, positive_sec3 + known.ahead_sec2, 0),
+         add.backward_sec3 = add.backward_sec2 + ifelse(known.behind_sec2 < abs(negative_sec3), abs(known.behind_sec2 + negative_sec3), 0),
+         known.ahead_sec3 = negative_sec3 + ifelse(known.ahead_sec2 + positive_sec3 > 0, 0, known.ahead_sec2 + positive_sec3),
+         #known.behind_sec3 = positive_sec3 + ifelse(draft_sec2 + negative_sec3 > 0, draft_sec2 + negative_sec3, 0),
+         known.behind_sec3 = positive_sec3 + ifelse(known.behind_sec2 + negative_sec3 > 0, known.behind_sec2 + negative_sec3, 0),
+         # section 4
+         draft_sec4 = ifelse(positive_sec4 + known.ahead_sec3 >= 0, positive_sec4 + known.ahead_sec3, 0),
+        
+         add.backward_sec4 = add.backward_sec3 + ifelse(known.behind_sec3 < abs(negative_sec4), abs(known.behind_sec3 + negative_sec4), 0),
+         
+         known.ahead_sec4 = negative_sec4 + ifelse(known.ahead_sec3 + positive_sec4 > 0, 0, known.ahead_sec3 + positive_sec4),
+         #known.behind_sec4 = positive_sec4 + ifelse(draft_sec3 + negative_sec4 > 0, draft_sec3 + negative_sec4, 0),
+         known.behind_sec4 = positive_sec4 + ifelse(known.behind_sec3 + negative_sec4 > 0, known.behind_sec3 + negative_sec4, 0)) %>% 
   select(date, alpha.code, contains("1"), contains("2"), contains("3"), contains("4")) 
 
 }
 
 
-
-
-
-calc_bay_sum <- function(df) {
-  
-  zcarried_forward <- df %>% 
-    rename(sec.prelim_4 = section.final_4) %>% 
-    mutate(birds2add = ifelse(sec.forward_4 < 0, abs(sec.forward_4), 0),
-           section.final_4 = sec.prelim_4 + birds2add,
-           bay.sum = section.final_1 + section.final_2 + section.final_3 + section.final_4)
-  
-}
 
 
 
