9b5dda316fa3db95bd08ca453e6ecb8814429640
scottfjennings <scott.f.jennings@gmail.com>
2022-01-24 08:57:51 -0800

    splitting now done at block level, not section level. this supports subsequent handling of nagatives using block level counts.

--- code/waterbird_cleaning2_split_groups.R
+++ code/waterbird_cleaning2_split_groups.R
@@ -3,36 +3,6 @@
 
 
 
-#' Expand pooled
-#'
-#' This expands grouped data so that there is a record for each constituent species.
-#'
-#' @param df data frame with section summed tallies and finals for each species
-#'
-#' @return data frame
-#' @export
-#'
-#' @examples
-expand_pooled <- function(df) {
-group_tallies_wide <- df %>% 
-  dplyr::filter(!is.na(group.spp)) %>% 
-  separate(group.spp, c(paste("group.spp", seq(1, 4))))
-
-group_tallies_long <- group_tallies_wide %>% 
-  pivot_longer(contains("group.spp")) %>% 
-  select(date, section, pooled.alpha.code = alpha.code, pooled.section.tally = section.tally, pooled.section.final = section.final, alpha.code = value) %>% 
-  filter(!is.na(alpha.code)) %>%
-  mutate(alpha.code = update_alpha(alpha.code))
-
-expanded_pooled <- df %>% 
-  filter(is.na(group.spp)) %>% 
-  select(-group.spp) %>% 
-  full_join(., group_tallies_long)%>%
-  mutate(across(contains("section.sum"), ~replace_na(., 0)))
-
-return(expanded_pooled)
-}
-
 
 
 #' Make pooled date constituents
@@ -118,6 +88,9 @@ return(df)
 
 
 #' Get pooled block tallies - CURRENTLY NOT USED
+#' 
+#' 
+#' Filter just data for pooled birds at the section scale and rename fields in prep for allocating to constituent species
 #'
 #' @param df data frame wbirds
 #'
@@ -125,6 +98,7 @@ return(df)
 #' @export
 #'
 #' @examples
+#' pooled_block <- get_pooled_block(wbirds)
 get_pooled_block <- function(df) {
 pooled <- df %>% 
   mutate(group.spp = translate_bird_names(alpha.code, "alpha.code", "group.spp")) %>% 
@@ -142,41 +116,25 @@ return(pooled)
 
 
 
-#' Title
-#'
-#' @param df data frame with section totals 
-#'
-#' @return data frame with summed section tallies and finals
-#' @export
-#'
-#' @examples
-get_pooled <- function(df) {
-pooled <- df %>% 
-  filter(!is.na(group.spp)) %>% 
-  select(-group.spp, -section.final) %>% 
-  rename(pooled.alpha.code = alpha.code,
-         pooled.section.tally = section.tally) %>% 
-  group_by(date, pooled.alpha.code) %>% 
-  mutate(pooled.bay.total = sum(pooled.section.tally)) %>% 
-  ungroup()
-return(pooled)
-}
 
-  # determine which ratio (section or bay) to allocate by
 
-#' Set allocation scale
+#' Allocate section POOLED birds to constituent species
 #' 
-#' Set allocation scale (section or bay) for any sections with pooled birds. 
+#' Set allocation scale (section or bay) for any sections with pooled birds based on the number of POOLED and unpooled birds in that section. 
 #'
-#' @param constituents_by_group data frame with the section and bay ratios of constituent species in each POOLED group for each day that POOLED group was recorded 
-#' @param pooled the data for all birds assigned to POOLED groups, summarized to section
+#' @param constituent_ratios data frame with the section and bay ratios of constituent species in each POOLED group for each day that POOLED group was recorded 
+#' @param pooled_block the data for all birds assigned to POOLED groupsat the block scale
 #'
 #' @return data frame
 #' @export
+#' 
+#' @details The allocation process yields non-integer values, which must be rounded to integer in some way. This rounding may lead to a different number of "allocated" birds than there were POOLED birds (generally either 1 more or 1 less). When there are 2 constituent species, rounding all values to the nearest integer is best, but when there are more than 2 constituent species than rounding DOWN may often be best. However, overall it seems best to apply 1 rounding rule to all records, regardless of constituent number. Rounding to the nearest integer yields the fewest extra or dropped birds. 
 #'
 #' @examples
-allocate_pooled <- function(constituents_by_group, pooled) {
-pooled_allocated <- right_join(constituents_by_group, pooled) %>% 
+#' allocated_pooled_block <- allocate_pooled_block(constituent_ratios, pooled_block)
+#' 
+allocate_pooled_block <- function(constituent_ratios, pooled_block) {
+pooled_allocated <- right_join(constituent_ratios, pooled_block) %>% 
   #select(-section.tally, -section.final, -bay.total) %>% 
   mutate(group.cut = ifelse(pooled.alpha.code %in% c("HEGR", "WCGR"), 50, 100),
          allocation.scale = NA) %>%
@@ -191,53 +149,58 @@ pooled_allocated <- right_join(constituents_by_group, pooled) %>%
                                           abs(bay.all.constituents) >= abs(pooled.section.tally) &  
                                           (abs(section.all.constituents) < abs(pooled.section.tally) |  abs(section.all.constituents) < group.cut) ~ "bay",
                                       TRUE ~ as.character(allocation.scale))) %>% 
-  # when there are fewer possitively IDed birds in entire bay than group cutoff, no allocation should be done
+  # when there are fewer positively IDed birds in entire bay than group cutoff, no allocation should be done
   mutate(allocation.scale = case_when(abs(pooled.section.tally) >= abs(bay.all.constituents) | group.cut >= abs(bay.all.constituents) ~ "no.allocation",
                                       TRUE ~ as.character(allocation.scale))) %>% 
   select(date, section, contains("pooled"), alpha.code, everything()) %>% 
-  mutate(allocated.tally = case_when(allocation.scale == "section" ~ pooled.section.tally * section.proportion,
-                               allocation.scale == "bay" ~ pooled.section.tally * bay.proportion,
+  mutate(allocated.count = case_when(allocation.scale == "section" ~ pooled.count * section.proportion,
+                               allocation.scale == "bay" ~ pooled.count * bay.proportion,
                                allocation.scale == "no.allocation" ~ 0),
-         allocated.tally = round(allocated.tally, 0))
+         allocated.count = round(allocated.count, 0)) %>% 
+  group_by(date, block, pooled.alpha.code) %>% 
+  mutate(total.allocated = sum(allocated.count)) %>% 
+  ungroup() %>% 
+  mutate(alloc.remainder = pooled.count - total.allocated)
 return(pooled_allocated)
 }
 
 
+  
 
-#' Title
+#' Combine the allocated and unallocated POOLED birds with the unpooled birds at the block scale
 #'
-#' @param section_tallies_finals output from sum_section_tallies_finals() with field group.spp added 
-#' @param allocated_pooled output from allocate_pooled()
+#' @param wbirds output from sum_section_tallies_finals() with field group.spp added 
+#' @param allocated_pooled_block output from allocate_pooled_block()
 #'
 #' @return data frame
 #' @export
 #'
 #' @examples
- combine_split_unsplit_unpooled <- function(section_tallies_finals, allocated_pooled) {
+#' split_unsplit_unpooled_block <- combine_split_unsplit_unpooled_block(wbirds, allocated_pooled_block)
+#' 
+ combine_split_unsplit_unpooled_block <- function(wbirds, allocated_pooled_block) {
 # combine allocated with data for birds IDed to species
 
-pooled_split <- allocated_pooled %>% 
-  filter(allocation.scale != "no allocation") %>% 
-  rename(count = allocated.tally) %>% 
-  sum_section_tallies_finals()
+pooled_split <- allocated_pooled_block %>% 
+  filter(allocation.scale != "no allocation", allocated.count != 0) %>% 
+  select(date, block, alpha.code, count = allocated.count, allocation.scale)
 
-pooled_unsplit <- allocated_pooled %>% 
+pooled_unsplit <- allocated_pooled_block %>% 
   filter(allocation.scale == "no.allocation") %>% 
-  select(date, section, alpha.code = pooled.alpha.code, count = section.tally)  %>% 
-  sum_section_tallies_finals()
+  select(date, block, alpha.code = pooled.alpha.code, count = pooled.count, allocation.scale)  %>%
+  distinct()
 
-unpooled <- section_tallies_finals %>% 
+unpooled <- wbirds %>% 
+  mutate(group.spp = translate_bird_names(alpha.code, "alpha.code", "group.spp")) %>% 
   filter(is.na(group.spp)) %>% 
-  select(-group.spp)
+  select(-group.spp, -section, -transect)
 
 wbirds_sppid_allocated <- bind_rows(unpooled, pooled_split, pooled_unsplit)  %>% 
-  group_by(date, section, alpha.code) %>% 
-  summarise(section.tally = sum(section.tally),
-            section.final = sum(section.final))
+  group_by(date, block, alpha.code) %>% 
+  summarise(count = sum(count)) %>% 
+    ungroup() %>% 
+    separate(block, c("transect", "section"), remove = FALSE)
 return(wbirds_sppid_allocated)
 }
-  
-  
-  
-  
+    
   
